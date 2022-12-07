400fb4fece29af0d07bd28a056253bd19111edbe
Oleksandr Vorobiov <ovor@softserveinc.com>
2022-12-07 15:57:10 +0200

    Roman to integer

---
+++ longestCommonPrefix/main.go
@@ -0,0 +1,13 @@
+package main
+
+func longestCommonPrefix(strs []string) string {
+  var prefix string
+  for
+  return prefix
+}
+
+func main() {
+  strs := []string{"flower", "flow", "flight"}
+
+  print(longestCommonPrefix(strs))
+}
--- romanToInt/main.go
+++ romanToInt/main.go
@@ -1,27 +1,32 @@
 package main
 
-import "strconv"
-
 func romanToInt(s string) int {
   var intFromRoman int = 0
   runes := []rune(s)
   for i := 0; i < len(runes); i++ {
-    print(s[i])
-    switch rSymbol := runes[i]; rSymbol {
+    switch rSymbol := string(runes[i]); rSymbol {
     case "M":
       intFromRoman += 1000
     case "D":
       intFromRoman += 500
     case "C":
-      intFromRoman += 100
+      if i+1 < len(runes) && (string(runes[i+1]) == "D" || string(runes[i+1]) == "M") {
+        intFromRoman -= 100
+      } else {
+        intFromRoman += 100
+      }
     case "L":
       intFromRoman += 50
     case "X":
-      intFromRoman += 10
+      if i+1 < len(runes) && (string(runes[i+1]) == "L" || string(runes[i+1]) == "C") {
+        intFromRoman -= 10
+      } else {
+        intFromRoman += 10
+      }
     case "V":
       intFromRoman += 5
     case "I":
-      if strconv.Itoa(int(s[i+1])) != "I" {
+      if i+1 < len(runes) && (string(runes[i+1]) == "V" || string(runes[i+1]) == "X") {
         intFromRoman -= 1
       } else {
         intFromRoman += 1
@@ -35,5 +40,5 @@ func romanToInt(s string) int {
 
 func main() {
   //print(string(179))
-  print(romanToInt("LVIII"))
+  print(romanToInt("MCMXCIV"))
 }
