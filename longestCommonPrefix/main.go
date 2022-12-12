d3268039b4f9cb2b25690793ede4de826987b225
Oleksandr Vorobiov <ovor@softserveinc.com>
2022-12-12 18:33:58 +0200

    LongestPrefix unfinished

--- longestCommonPrefix/main.go
+++ longestCommonPrefix/main.go
@@ -1,13 +1,31 @@
 package main
 
 func longestCommonPrefix(strs []string) string {
-  var prefix string
-  for
-  return prefix
+  var prefix []uint8
+  isCommonPrefix := true
+  var curChar uint8
+  var i = 0
+  for isCommonPrefix {
+    if curChar != 0 {
+      prefix = append(prefix, curChar)
+    }
+    curChar = 0
+    for _, word := range strs {
+      if curChar == 0 && len(word) > 0 {
+        curChar = word[i]
+      } else {
+        if len(word) == 0 || word[i] != curChar {
+          isCommonPrefix = false
+        }
+      }
+    }
+    i++
+  }
+  return string(prefix)
 }
 
 func main() {
-  strs := []string{"flower", "flow", "flight"}
+  strs := []string{"a"}
 
   print(longestCommonPrefix(strs))
 }
