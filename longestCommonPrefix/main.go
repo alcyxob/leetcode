606ee6bd6934171090beaf15e6eb77a73807aef9
Oleksandr Vorobiov <ovor@softserveinc.com>
2022-12-13 10:12:55 +0200

    longest common prefix solved

--- longestCommonPrefix/main.go
+++ longestCommonPrefix/main.go
@@ -1,31 +1,40 @@
 package main
 
 func longestCommonPrefix(strs []string) string {
-  var prefix []uint8
-  isCommonPrefix := true
-  var curChar uint8
-  var i = 0
-  for isCommonPrefix {
-    if curChar != 0 {
-      prefix = append(prefix, curChar)
-    }
-    curChar = 0
-    for _, word := range strs {
-      if curChar == 0 && len(word) > 0 {
-        curChar = word[i]
-      } else {
-        if len(word) == 0 || word[i] != curChar {
-          isCommonPrefix = false
-        }
-      }
-    }
-    i++
-  }
-  return string(prefix)
+	var prefix []uint8
+	isCommonPrefix := true
+	var curChar uint8
+	var i = 0
+	var minLen = 0
+	for isCommonPrefix {
+		if curChar != 0 {
+			prefix = append(prefix, curChar)
+		}
+		curChar = 0
+		for _, word := range strs {
+
+			if minLen == 0 && len(word) != 0 {
+				minLen = len(word)
+			}
+			if minLen > len(word) {
+				minLen = len(word)
+			}
+
+			if curChar == 0 && i < minLen {
+				curChar = word[i]
+			} else {
+				if len(word) == 0 || minLen <= i || word[i] != curChar {
+					isCommonPrefix = false
+				}
+			}
+		}
+		i++
+	}
+	return string(prefix)
 }
 
 func main() {
-  strs := []string{"a"}
+	strs := []string{"dog", "racecar", "car"}
 
-  print(longestCommonPrefix(strs))
+	print(longestCommonPrefix(strs))
 }
